f8d748cba211567d021820db425948669694a847
entbappy <entbappy73@gmail.com>
2023-06-05 12:59:38 +0600

    requirements added

--- template.py
+++ template.py
@@ -26,12 +26,12 @@ list_of_files = [
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
+    "reseach/trials.ipynb",
     "templates/index.html",
     "app.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "tests.py"
 ]
 
 
